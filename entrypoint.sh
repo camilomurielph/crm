e2c26d49542fb54f0fe96aeaeb804f46a5e32636
camilomurielph <149724435+camilomurielph@users.noreply.github.com>
2026-09-09 17:57:26 -0500

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+python manage.py migrate
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+for username, password in [('camilomuriel', 'b55f86bd4c353'), ('andresfel', 'BDA98@')]:
+    user, created = User.objects.get_or_create(username=username)
+    if created:
+        user.set_password(password)
+        user.save()
+        print(f'Usuario {username} creado.')
+    else:
+        print(f'Usuario {username} ya existe.')
+"
+exec gunicorn --bind 0.0.0.0:8000 actols_crm.wsgi:application
