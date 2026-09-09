cb2386bacef559d7d059712e67ca65843a5b9491
camilomurielph <149724435+camilomurielph@users.noreply.github.com>
2026-09-09 18:50:32 -0500

    Update settings.py

--- actols_crm/settings.py
+++ actols_crm/settings.py
@@ -52,7 +52,7 @@ WSGI_APPLICATION = 'actols_crm.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.environ.get('DB_PATH', BASE_DIR / 'db.sqlite3'),
+        'NAME': os.environ.get('DB_PATH', BASE_DIR / '/data/db.sqlite3'),
         'OPTIONS': {'timeout': 20},
     }
 }
