bb477d070a5ce511f380659702fdf0a07b4dc5d9
camilomurielph <149724435+camilomurielph@users.noreply.github.com>
2026-09-09 18:47:39 -0500

    Update settings.py

--- actols_crm/settings.py
+++ actols_crm/settings.py
@@ -76,7 +76,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-LOGIN_URL = '/login/'
+LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
 
